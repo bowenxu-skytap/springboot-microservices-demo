ef648763b662a7184192921f675e4459d6d10271
Bowen Xu <jimmyxbw@gmail.com>
2019-04-06 16:57:48 -0400

    Use WebClient to make API calls

--- MovieCatalogService/src/main/java/bowen/resources/MovieCatalogResource.java
+++ MovieCatalogService/src/main/java/bowen/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import bowen.models.CatalogItem;
 import bowen.models.Movie;
@@ -20,6 +21,9 @@ public class MovieCatalogResource {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -33,6 +37,14 @@ public class MovieCatalogResource {
 
 		return ratings.stream().map(rating -> {
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			/*
+			Movie movie = webClientBuilder.build()
+								.get()
+								.uri("http://localhost:8082/movies/" + rating.getMovieId())
+								.retrieve()
+								.bodyToMono(Movie.class)
+								.block();
+			*/
 			return new CatalogItem(movie.getName(), "Robot fight", rating.getRating());
 		})
 		.collect(Collectors.toList());
